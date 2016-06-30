2d0c6a449fe9d3e13d28796d897dcf4ef52d8c08
BlackCritical <lvlup@t-online.de>
2016-06-30 15:31:27 +0200

    Timer Added(not working yet)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,43 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 public class Controller {
-    //@FXML
+
     public Button nextStep = new Button("Nächster Schritt");
     public Button reworkTest = new Button("Test korrigieren");
     public Label testOverview = new Label("Blala");
     public Label codeOverview = new Label("Blala");
     public TextArea writeHere = new TextArea("Write your Code here");
     public HBox buttonBox = new HBox();
+    public static Label timerLabel = new Label();
+
+    static volatile SimpleIntegerProperty i = new SimpleIntegerProperty(0);
+    static volatile boolean run = true;
+    static volatile int k = 0;
 
 
-    public void start(Stage primaryStage) throws Exception {
+    public static void createTimer(){
+        run=true;
+        timerLabel.textProperty().bind(i.asString());
 
+        Thread t = new Thread(() -> {
+            while (run) {
+                k++;
+                Platform.runLater(() -> i.set(k));
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,12 +8,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Controller.createTimer();
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
 
-        primaryStage.setScene(new Scene(root, 1000,1000));
+        primaryStage.setOnCloseRequest(event -> Controller.run = false);
+
+        primaryStage.setScene(new Scene(root, 1000,800));
         primaryStage.show();
     }
 
