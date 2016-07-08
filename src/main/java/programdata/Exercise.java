2b6d947e48ceb73d773bde7c111655e4d737948d
toque100 <tobias.quest@hhu.de>
2016-07-08 18:05:57 +0200

    travis Problemlösung durch Update?

--- src/main/java/programdata/Exercise.java
+++ src/main/java/programdata/Exercise.java
@@ -63,7 +63,7 @@ public class Exercise {
 			this.compileFolder = CompilerFactory.getCompiler(this.code, this.test);
 			compileFolder.compileAndRunTests();
 			//if(hasC)
-				// TODO hier fehlt noch was mehr
+				// TODO hier fehlt noch was
 		}
 	}
 
